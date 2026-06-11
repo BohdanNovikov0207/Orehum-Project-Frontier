f83bbb73e41b3db89057246f9269e77f18e8368f
BohdanNovikov0207 <bogdanblacktop@gmail.com>
2026-06-11 21:22:44 +0300

    Genders in manifest

--- Content.Client/Stylesheets/StyleNano.cs
+++ Content.Client/Stylesheets/StyleNano.cs
@@ -189,6 +189,8 @@ namespace Content.Client.Stylesheets
         //Bwoink
         public const string StyleClassPinButtonPinned = "pinButtonPinned";
         public const string StyleClassPinButtonUnpinned = "pinButtonUnpinned";
+        //Delta-V - Manifest pronouns
+        public const string StyleClassCrewManifestGender = "CrewManifestGender";
 
 
         public override Stylesheet Stylesheet { get; }
@@ -1444,6 +1446,12 @@ namespace Content.Client.Stylesheets
                     .Class(StyleClassItemStatusNotHeld)
                     .Prop("font", notoSansItalic10)
                     .Prop("font-color", ItemStatusNotHeldColor),
+
+                // Delta-V add style for pronouns on the crew manifest
+                Element<RichTextLabel>()
+                    .Class(StyleClassCrewManifestGender)
+                    .Prop("font", notoSansItalic10)
+                    .Prop("font-style", "italic"),
 
                 Element<RichTextLabel>()
                     .Class(StyleClassItemStatus)
