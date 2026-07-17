1f672cc98f5e0cc1fd600d6be54d4b24e3e9c444
Bogdan Novikov <150541068+BohdanNovikov0207@users.noreply.github.com>
2026-07-17 13:30:00 +0300

    Retexture (#4)
    * Thrusters
    
    * ShipRepairTool tags
    
    * TheGypsyBaron resprite, v1
    
    * TheGypsyBaron resprite, v2
    
    * Firelock sprites
    
    * License name changed
    
    * Changed sounds, etc
    
    * Remove stellar assets
    
    * Remove Stellar texturs and protos

--- Content.Server/Atmos/Monitor/Components/AtmosAlarmableComponent.cs
+++ Content.Server/Atmos/Monitor/Components/AtmosAlarmableComponent.cs
@@ -36,7 +36,7 @@ public sealed partial class AtmosAlarmableComponent : Component
     [ViewVariables] public bool IgnoreAlarms { get; set; } = false;
 
     [DataField("alarmSound")]
-    public SoundSpecifier AlarmSound { get; set; } = new SoundPathSpecifier("/Audio/_ST/Machines/atmos_alarm.ogg"); // Stellar - New default Atmos alarm
+    public SoundSpecifier AlarmSound { get; set; } = new SoundPathSpecifier("/Audio/_Orehum/Machines/atmos_alarm.ogg"); // Orehum - New default Atmos alarm
 
     [DataField("alarmVolume")]
     public float AlarmVolume { get; set; } = -10;
